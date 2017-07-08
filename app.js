abf4e563345a866285ab58b232c51ba910e0eec8
Allie Grampa <agrampa@yahoo.com>
2017-07-08 15:22:10 -0700

    adding component and rendering

---
+++ app.js
@@ -0,0 +1,14 @@
+let Hello = React.createClass({
+  render: function() {
+    return (
+      // createElement is a helper method
+      // React.createElement('div', null, 'Hello World!')
+      // instead of createElement, do this instead:
+      <div>Hello World!</div>  // use JSX transformer along with Gulp and Grunt, otherwise in-browser transformer will be too slow on a large-scale program
+    )
+  }
+})
+
+//reactDom is a package, allows interaction with browser DOM
+// two args: component to render, and DOM node to attach to
+ReactDOM.render(<Hello />, document.getElementById('container'));
